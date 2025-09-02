3b324e63302f452106641211011c811effaedf48
Alejandro Uribe <uriberalejandro+github@gmail.com>
2025-09-01 20:01:00 -0400

    Fix database settings to use DATABASE_URL

--- cit390Project/settings.py
+++ cit390Project/settings.py
@@ -40,16 +40,8 @@ DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = [env("ALLOWED_HOSTS")]
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': env('DATABASE_PORT'),        
-    }
+    'default': env.db_url('DATABASE_URL'),
 }
 
 # Application definition
